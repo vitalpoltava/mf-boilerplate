b2b5607c8b5903ab919de09abc4eeeb88b21955c
Vitaliy Omelkin <vitalii.omelkin@teladochealth.com>
2022-07-26 22:54:44 +0300

    Fix layout

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { createRoot } from 'react-dom/client';
 import HostApp from "./Host";
 import { loadFederatedModule } from "./loadFederatedModule";
+import Config from "./configs";
 
-const url = `http://localhost:3001/remoteEntry.js`;
-const RemoteApp = React.lazy(loadFederatedModule( url, 'remoteapp', './RemoteRoot'));
+// Load remote fragment
+const RemoteApp = React.lazy(loadFederatedModule(Config.REMOTE_URL, 'remoteapp', './RemoteRoot'));
 
 import "./index.css";
 
 const App = () => (
   <table className="main-table">
     <thead>
-      <th>Host app</th>
-      <th>remote app</th>
+      <tr>
+        <th>Host app</th>
+        <th>remote app</th>
+      </tr>
     </thead>
     <tbody>
       <tr>
@@ -20,7 +23,9 @@ const App = () => (
           <HostApp />
         </td>
         <td>
-          <RemoteApp />
+          <Suspense fallback={<div>Loading...</div>}>
+            <RemoteApp />
+          </Suspense>
         </td>
       </tr>
     </tbody>
---
+++ host/src/configs.ts
@@ -0,0 +1,5 @@
+const Config = {
+    REMOTE_URL: "http://localhost:3001/remoteEntry.js"
+}
+
+export default Config;
