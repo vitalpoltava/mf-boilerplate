ae08f8f91163a364ce7ae862add16efe2794a9e1
Vitaliy Omelkin <vitalii.omelkin@teladochealth.com>
2022-08-05 15:15:04 +0300

    rename folders, add mock form

--- ext-form/src/App.tsx
+++ ext-form/src/App.tsx
@@ -2,8 +2,13 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import FormWrapper from "./FormWrapper";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
-const App = () => (<FormWrapper name="Local Form Run" />);
+const App = () => (
+  <div className="p-5">
+    <FormWrapper name="Local Form Run" />
+  </div>
+);
 const root = createRoot(document.getElementById("app")!);
 root.render(<App />);
--- form/src/FormWrapper.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FormWrapper = ({name}: any) => (
-  <div>{name}</div>
-)
-
-export default FormWrapper;
